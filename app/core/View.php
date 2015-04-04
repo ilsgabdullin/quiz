29947b552166110ead2bcaf5056e4b49193b5e1c
Ильсур Габдуллин <ilsgabdullin@gmail.com>
2015-04-04 19:06:06 +0300

    Добавлена документация для класса View

--- app/core/View.php
+++ app/core/View.php
@@ -3,16 +3,34 @@ namespace app\core;
 
 use app\core\Quiz;
 
+/**
+ * Класс для работы с представлениями.
+ * @package app\core
+ * @author Ильсур Габдуллин <ilsgabdullin@gmail.com>
+ */
 class View
 {
-
+    /**
+     * @var string шаблон представления
+     */
     public $layout = 'layouts/main';
 
+    /**
+     * Формирует URL.
+     * @param string $route внутренний маршрут (например, 'main/index')
+     * @param array $params параметры запроса
+     * @return string URL с параметрами запроса
+     */
     public static function url($route, $params = [])
     {
         return Quiz::app()->url($route, $params);
     }
 
+    /**
+     * Формирует вывод представления.
+     * @param string $view полный путь к файлу с представлением
+     * @param array $_data переменные, используемые в отображении
+     */
     public function generate($view, $_data = null)
     {
         if (is_array($_data)) {
@@ -27,16 +45,30 @@ class View
         include ROOT . 'app/views/' . $this->layout . '.php';
     }
 
+    /**
+     * Возвращает название текущего контроллер.
+     * @return string название контроллера
+     */
     public function getController()
     {
         return Quiz::app()->getController();
     }
 
+    /**
+     * Экранирует специальные символы.
+     * @param string $text строка для экранирования
+     * @return string экранированная строка
+     */
     public function encode($text)
     {
         return htmlspecialchars($text, ENT_QUOTES, 'utf-8');
     }
 
+    /**
+     * Проверяет переменную.
+     * @param mixed $value переменная
+     * @return mixed|null
+     */
     public function check($value)
     {
         return !empty($value) ? $value : null;
