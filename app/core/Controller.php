95cbb1f59d932619fd12c8fcf6e4e6f7389f2b50
Ильсур Габдуллин <ilsgabdullin@gmail.com>
2015-04-04 18:59:44 +0300

    Добавлена документация для класса Controller

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,50 +1,86 @@
 <?php
 namespace app\core;
 
+/**
+ * Класс контроллера.
+ * @package app\core
+ * @author Ильсур Габдуллин <ilsgabdullin@gmail.com>
+ */
 class Controller
 {
-
+    /**
+     * @var View ссылка на объект для работы с представлением
+     */
+    public $view;
+    /**
+     * @var string действие по умолчанию
+     */
+    public $defaultAction = 'index';
+    /**
+     * @var string название контроллера
+     */
     private $_id;
 
-    public $model;
-    public $view;
-    public $defaultAction = 'index';
-
+    /**
+     * Конструктор.
+     * @param string $id название контроллера
+     */
     function __construct($id)
     {
         $this->view = new View();
         $this->_id = $id;
     }
 
-    public function getId()
-    {
-        return $this->_id;
-    }
-
+    /**
+     * Формирует полное название метода с действием.
+     * @param string $action название метода
+     * @return string полное название метода действия
+     */
     public function getActionMethod($action)
     {
-        if (!$action)
+        if (!$action) {
             $action = $this->defaultAction;
+        }
 
         $action = 'action' . ucfirst($action);
 
         return $action;
     }
 
-    public function getViewPath($view)
-    {
-        if (!is_string($view))
-            return false;
-
-        if ($view[0] != '/')
-            $view = $this->getId() . DIRECTORY_SEPARATOR . $view;
-
-        return $view . '.php';
-    }
-
+    /**
+     * Выводит представление.
+     * @param string $view название файла с представлением
+     * @param array $data данные для передачи в представление
+     */
     public function render($view, $data = [])
     {
         $view = $this->getViewPath($view);
         $this->view->generate($view, $data);
+    }
+
+    /**
+     * Возвращает путь к представлению.
+     * @param string $view название файла с представленеим
+     * @return bool|string путь к файлу с представлением
+     */
+    public function getViewPath($view)
+    {
+        if (!is_string($view)) {
+            return false;
+        }
+
+        if ($view[0] != '/') {
+            $view = $this->getId() . DIRECTORY_SEPARATOR . $view;
+        }
+
+        return $view . '.php';
+    }
+
+    /**
+     * @return string название контроллера
+     */
+    public function getId()
+    {
+        return $this->_id;
     }
 }
