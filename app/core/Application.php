7a0f72aeea0ad16682f7b73e490fee7171e3398a
Ильсур Габдуллин <ilsgabdullin@gmail.com>
2015-04-04 12:25:48 +0300

    Документация к app/Application.php

--- app/core/Application.php
+++ app/core/Application.php
@@ -1,18 +1,53 @@
 <?php
 namespace app\core;
 
+/**
+ * Базовый класс приложения.
+ * @package app\core
+ * @author Ильсур Габдуллин <ilsgabdullin@gmail.com>
+ */
+
 class Application
 {
-
+    /**
+     * @var string контроллер по умолчанию
+     */
+    public $defaultController = 'Main';
+    /**
+     * @var string базовая директория приложения.
+     * Если приложенеи устанавливается в подпапку,
+     * то необходимо указать эту папку.
+     */
     protected $baseURL = '/web';
+    /**
+     * @var string неймспейс для контроллеров приложения.
+     * Используется при создании контроллера.
+     */
     protected $controllersPath = 'app\\controllers\\';
+    /**
+     * @var string страница для вывод ошибки
+     */
     protected $errorPage = 'main/error';
+    /**
+     * @var \PDO объект для работы с базой данных
+     * Доступно из любой точки приложения через Quiz::app()->db
+     */
     protected $db = null;
+    /**
+     * @var Config доступ к конфигурации приложегния
+     */
     protected $config;
+    /**
+     * @var string контроллер, создаваемый при создании приложения
+     */
     protected $_controller;
 
-    public $defaultController = 'Main';
-
+    /**
+     * Конструктор.
+     *
+     * @param Config $config передается объект для работы с конфигурацией
+     * @throws \Exception если не найдено запрашиваемое действие
+     */
     public function __construct(Config $config)
     {
         Quiz::setApplication($this);
@@ -33,6 +68,10 @@ class Application
         }
     }
 
+    /**
+     * Парсит URL-запрос и возвращает название контроллера и действие.
+     * @return array возвращается массив [0=>'Контроллер', 1=>'Действие']
+     */
     private function parseRoute()
     {
         $controller = $this->defaultController;
@@ -56,6 +95,10 @@ class Application
         return ['controller' => $controller, 'action' => $action];
     }
 
+    /**
+     * Соединяется с базой данных и возвращается ссылку на созданный объект.
+     * @return \PDO объект для работы с базой данных
+     */
     public function db()
     {
         if ($this->db == null) {
@@ -71,11 +114,35 @@ class Application
         return $this->db;
     }
 
+    /**
+     * @return string название текущего контроллера
+     */
     public function getController()
     {
         return $this->_controller;
     }
 
+    /**
+     * Перенаправляет пользователя на другой URL.
+     * @param string $route путь к странице вида 'контроллер/действие'
+     * @param bool $terminate завершить выполнение приложения
+     * @param int $statusCode HTTP-код, возвращаемый в заголовке
+     */
+    public function redirect($route, $terminate = true, $statusCode = 302)
+    {
+        header('Location: ' . $this->url($route), true, $statusCode);
+
+        if ($terminate) {
+            exit();
+        }
+    }
+
+    /**
+     * Формирует нормализованный URL-адрес
+     * @param string $route путь к странице вида 'контроллер/действие'
+     * @param array $params параметры запроса
+     * @return string нормализованный URL-адрес
+     */
     public function url($route, $params = [])
     {
         $url = $this->baseURL . '/';
@@ -95,14 +162,5 @@ class Application
         }
 
         return $url;
-    }
-
-    public function redirect($route, $terminate = true, $statusCode = 302)
-    {
-        header('Location: ' . $this->url($route), true, $statusCode);
-
-        if ($terminate) {
-            exit();
-        }
     }
 }
