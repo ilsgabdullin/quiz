a59d0cf10a9279f75d326671f7d9c03ad75b09f7
Ильсур Габдуллин <ilsgabdullin@gmail.com>
2015-04-04 18:53:39 +0300

    Добавлена документация для класса Config

--- app/core/Config.php
+++ app/core/Config.php
@@ -1,17 +1,35 @@
 <?php
 namespace app\core;
 
+/**
+ * Класс для работы с конфигурацией приложения.
+ * @package app\core
+ * @author Ильсур Габдуллин <ilsgabdullin@gmail.com>
+ */
 class Config
 {
-
+    /**
+     * @var array данные конфигурации
+     */
     private $_data = [];
 
+    /**
+     * Конструктор.
+     * @param array $config с данными конфигураци
+     */
     function __construct($config)
     {
-        if (is_array($config))
+        if (is_array($config)) {
             $this->_data = $config;
+        }
     }
 
+    /**
+     * Возвращает конфигурационные данные по заданному пути.
+     * Например, 'db.dsn' вернёт $config['db']['dsn'].
+     * @param string $path путь к данным
+     * @return array|string|bool данные конфигурации
+     */
     public function get($path)
     {
         $value = $this->_data;
